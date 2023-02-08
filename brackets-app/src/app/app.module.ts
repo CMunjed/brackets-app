9a7d79624f2a4f6ec2225428740db31c9e0d9567
loganbialek <93264997+loganbialek@users.noreply.github.com>
2023-02-08 14:05:27 -0500

    Updated formating and added some images

--- brackets-app/src/app/app.module.ts
+++ brackets-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
