9a3d7b0566e9f7f7253c47264d09d9bc793fd8bc
Shawn Banks <shawnbanks@ufl.edu>
2023-03-01 19:45:41 -0500

    got cypress running again and built click test

--- brackets-app/cypress/e2e/spec.cy.ts
+++ brackets-app/cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
 describe('template spec', () => {
+  // open web application on local host
   it('successfully loads', () => {
-    cy.visit('/')
+    cy.visit('http://localhost:4200/')
+  })
+  // test click on sign in button
+  it('passes click test 1', () => {
+    cy.contains('Sign in').click()
   })
 })
