eb2d6f39e47e2ef2c2f0bae4a5add1115ad0745e
Shawn Banks <shawnbanks@ufl.edu>
2023-03-29 19:43:48 -0400

    intitial unit tests working, adding more

--- brackets-app/cypress/e2e/spec.cy.ts
+++ brackets-app/cypress/e2e/spec.cy.ts
@@ -5,8 +5,25 @@ describe('template spec', () => {
   it('successfully loads', () => {
     cy.visit('http://localhost:4200/')
   })
-  // test click on sign in button
+
+  
+  /*
+  // test text enter in box
+  it('passes text enter test', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('input').type('8')
+  })
+  */
+
+  // test click on add teams button
   it('passes click test 1', () => {
+    cy.visit('http://localhost:4200/')
     cy.contains('Add Teams').click()
+  })
+
+  // test click on sign in button
+  it('passes click test 2', () => {
+    cy.visit('http://localhost:4200/')
+    cy.contains('Sign in with Google').click()
   })
 })
